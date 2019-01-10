5dca3269288e69958a5121eaa5255eb488f712a8
20411lyj <lpdt0079@naver.com>
2019-01-10 19:42:18 +0900

    0110 자바수업

---
+++ js/2019.01.js
@@ -0,0 +1,28 @@
+
+$("#bt").click(function(){
+	var html = `<div class="box">훗!</div>`;
+	$("#box").html($("#box").html() + html);
+});
+
+$("#bt2").click(function(){
+	$(".box").css({"width":"200px", "height":"200px", "float":"left"});
+});
+
+/* var i =1;
+$("#bt3").click(function(){
+    $(".box2").append(`<div>${i++}</div>`);
+
+}); */
+
+$("#bt3").click(function(){
+    $("box2").empty();
+    var num = new Array();
+    for(var i = 0; i<45; i++){
+        num[i] = i + 1 + "번";
+        $(".box2").append(`<div>${num[i]}</div>`);
+
+    }
+    for(var i in num){
+        
+    }
+});
